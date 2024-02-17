edae69479788f91ab26ef5534ab3401123fc2c23
HaniChihabi <hanichihabih@gmail.com>
2024-02-17 18:05:47 +0100

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,196 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+import { ContactSection } from '@/components/ContactSection'
+import { Container } from '@/components/Container'
+import { FadeIn, FadeInStagger } from '@/components/FadeIn'
+import { List, ListItem } from '@/components/List'
+import { SectionIntro } from '@/components/SectionIntro'
+import { StylizedImage } from '@/components/StylizedImage'
+import { Testimonial } from '@/components/Testimonial'
+import logoBrightPath from '@/images/clients/bright-path/logo-light.svg'
+import logoFamilyFund from '@/images/clients/family-fund/logo-light.svg'
+import logoGreenLife from '@/images/clients/green-life/logo-light.svg'
+import logoHomeWork from '@/images/clients/home-work/logo-light.svg'
+import logoMailSmirk from '@/images/clients/mail-smirk/logo-light.svg'
+import logoNorthAdventures from '@/images/clients/north-adventures/logo-light.svg'
+import logoPhobiaDark from '@/images/clients/phobia/logo-dark.svg'
+import logoPhobiaLight from '@/images/clients/phobia/logo-light.svg'
+import logoUnseal from '@/images/clients/unseal/logo-light.svg'
+import imageLaptop from '@/images/laptop.jpg'
+import imageMe from '@/images/me.jpeg'
+import { DownCircleOutlined } from '@ant-design/icons';
+
+
+import { loadCaseStudies } from '@/lib/mdx'
+
+const clients = [
+  ['Phobia', logoPhobiaLight],
+  ['Family Fund', logoFamilyFund],
+  ['Unseal', logoUnseal],
+  ['Mail Smirk', logoMailSmirk],
+  ['Home Work', logoHomeWork],
+  ['Green Life', logoGreenLife],
+  ['Bright Path', logoBrightPath],
+  ['North Adventures', logoNorthAdventures],
+]
+
+
+
+function CaseStudies({ caseStudies }) {
+  return (
+    <>
+      <SectionIntro
+        title="Harnessing technology for a brighter future"
+        className="mt-24 sm:mt-32 lg:mt-40"
+      >
+        <p>
+          We believe technology is the answer to the world’s greatest
+          challenges. It’s also the cause, so we find ourselves in bit of a
+          catch 22 situation.
+        </p>
+      </SectionIntro>
+      <Container className="mt-16">
+        <FadeInStagger className="grid grid-cols-1 gap-8 lg:grid-cols-3">
+          {caseStudies.map((caseStudy) => (
+            <FadeIn key={caseStudy.href} className="flex">
+              <article className="relative flex w-full flex-col rounded-3xl p-6 ring-1 ring-neutral-950/5 transition hover:bg-neutral-50 sm:p-8">
+                <h3>
+              
+                </h3>
+                <p className="mt-6 flex gap-x-2 text-sm text-neutral-950">
+                  <time
+                    dateTime={caseStudy.date.split('-')[0]}
+                    className="font-semibold"
+                  >
+                    {caseStudy.date.split('-')[0]}
+                  </time>
+                  <span className="text-neutral-300" aria-hidden="true">
+                    /
+                  </span>
+                  <span>Case study</span>
+                </p>
+                <p className="mt-6 font-display text-2xl font-semibold text-neutral-950">
+                  {caseStudy.title}
+                </p>
+                <p className="mt-4 text-base text-neutral-600">
+                  {caseStudy.description}
+                </p>
+              </article>
+            </FadeIn>
+          ))}
+        </FadeInStagger>
+      </Container>
+    </>
+  )
+}
+
+function Services() {
+  return (
+    <>
+      <SectionIntro
+        eyebrow="Services"
+        title="We help you identify, explore and respond to new opportunities."
+        className="mt-24 sm:mt-32 lg:mt-40"
+      >
+        <p>
+          As long as those opportunities involve giving us money to re-purpose
+          old projects — we can come up with an endless number of those.
+        </p>
+      </SectionIntro>
+      <Container className="mt-16">
+        <div className="lg:flex lg:items-center lg:justify-end">
+          <div className="flex justify-center lg:w-1/2 lg:justify-end lg:pr-12">
+            <FadeIn className="w-[33.75rem] flex-none lg:w-[45rem]">
+              <StylizedImage
+                src={imageLaptop}
+                sizes="(min-width: 1024px) 41rem, 31rem"
+                className="justify-center lg:justify-end"
+              />
+            </FadeIn>
+          </div>
+          <List className="mt-16 lg:mt-0 lg:w-1/2 lg:min-w-[33rem] lg:pl-4">
+            <ListItem title="Web development">
+              We specialise in crafting beautiful, high quality marketing pages.
+              The rest of the website will be a shell that uses lorem ipsum
+              everywhere.
+            </ListItem>
+            <ListItem title="Application development">
+              We have a team of skilled developers who are experts in the latest
+              app frameworks, like Angular 1 and Google Web Toolkit.
+            </ListItem>
+            <ListItem title="E-commerce">
+              We are at the forefront of modern e-commerce development. Which
+              mainly means adding your logo to the Shopify store template we’ve
+              used for the past six years.
+            </ListItem>
+            <ListItem title="Custom content management">
+              At Studio we understand the importance of having a robust and
+              customised CMS. That’s why we run all of our client projects out
+              of a single, enormous Joomla instance.
+            </ListItem>
+          </List>
+        </div>
+      </Container>
+    </>
+  )
+}
+
+export const metadata = {
+  description:
+    'We are a development studio working at the intersection of design and technology.',
+}
+
+export default async function Home() {
+  let caseStudies = (await loadCaseStudies()).slice(0, 3)
+
+  return (
+    <>
+    <section>
+    <div className='h-screen flex flex-col lg:flex-row items-center'>
+        {/* Content Container */}
+        <div className="flex-1 flex items-center justify-center relative bottom-44 left-44 mt-5 ml-5">
+          <div className="max-w-3xl">
+            {/* Your text content here */}
+            <h1 className="font-display text-8xl font-medium tracking-tight text-neutral-950 sm:text-7xl ">
+              Hani Chihabi
+            </h1>
+            <p className="mt-8 text-3xl text-neutral-600 leading-normal">
+              Problem solver living in Germany <br /> Passionate about software development <br /> Eager to learn and innovate <br />I focus on crafting effective solutions.
+            </p>
+            <DownCircleOutlined className='text-[80px] text-center relative left-64 top-20 text-slate-400 hover:text-black'/>
+          </div>
+        </div>
+
+        {/* Image Container */}
+        <div className="flex-1 h-full flex items-center justify-center">
+          <div className="relative right-32 top-56 h-full w-full max-w-lg">
+          <div className='flex-1 h-1/2 relative rounded-xl overflow-hidden'>
+              <Image 
+                src={imageMe} // Adjust the path to your image
+                alt="A descriptive alt text"
+                layout="fill"
+                objectFit="cover" // This will cover the available space, similar to bg-cover
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      </section>
+
+      
+
+      <Testimonial className=" relative bottom-64"
+        
+      >
+        The team at Studio went above and beyond with our onboarding, even
+        finding a way to access the user’s microphone without triggering one of
+        those annoying permission dialogs.
+      </Testimonial>
+
+      <Services />
+
+      <ContactSection />
+    </>
+  )
+}
