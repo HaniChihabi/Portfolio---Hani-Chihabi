b6fa5b3b87beb28ccf9c635ce6b12f3b1ceb0c22
HaniChihabi <hanichihabih@gmail.com>
2024-03-05 14:25:54 +0100

    new home page

--- src/app/[locale]/page.jsx
+++ src/app/[locale]/page.jsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 import { FadeIn, FadeInStagger } from './components/FadeIn'
 import imageMe from './images/kolnn.jpeg'
 import { Border } from './components/Border'
-import { SocialMediaNew } from './components/SocialMediaNew'
+import { SocialMedia } from './components/SocialMedia'
 import React from 'react';
 import {useTranslations} from 'next-intl';
 import { Container } from './components/Container'
@@ -10,9 +10,9 @@ import NewModal from './components/Modalnew'
 export default function Home() {
 const t = useTranslations('home');
   return (
-      <section className="overflow-hidden h-[80vh]">
+      <section className="overflow-hidden h-[90vh]">
         
-      <FadeIn className='  block lg:flex-row items-center justify-center overflow-hidden relative top-40'>
+      <FadeIn className='  block lg:flex-row items-center justify-center overflow-hidden relative top-52'>
         {/* Content Container */}
         
         <div className="flex-1 flex items-center justify-center ml-5">
@@ -21,16 +21,18 @@ const t = useTranslations('home');
             {t('header1')}
             </h1> */}
             {/* <NewModal/> */}
-            <h1 className="font-display font-normal tracking-normal text-neutral-950 lg:text-7xl md:text-6xl sm:text-6xl mb-5">
+            <h1 className="font-display font-normal tracking-normal text-neutral-950 lg:text-7xl md:text-6xl sm:text-6xl mb-2">
               {t('header2')}
             </h1>
 
-            <p className="font-display top-5 text-neutral-600 lg:leading-relaxed md:leading-relaxed sm:leading-relaxed lg:pb-0 sm:pb-8 font-light lg:text-3xl md:text-2xl sm:text-2xl relative mb-14">
+            <p className=" text-neutral-600 lg:leading-relaxed md:leading-relaxed sm:leading-relaxed lg:text-2xl md:text-2xl sm:text-2xl relative left-0 bottom-9 lg:pb-0 sm:pb-8 mt-14">
             {t('intro1')}
             </p>
-
-            <p className="font-display text-neutral-600 lg:leading-relaxed md:leading-relaxed sm:leading-relaxed font-medium lg:text-3xl md:text-2xl sm:text-2xl relative left-0 bottom-9 lg:pb-0 sm:pb-8">
+            <p className=" text-neutral-600 lg:leading-relaxed md:leading-relaxed sm:leading-relaxed lg:text-2xl md:text-2xl sm:text-2xl relative left-0 bottom-9 lg:pb-0 sm:pb-8">
             {t('intro2')}
+            </p>
+            <p className="font-display text-neutral-600 lg:leading-relaxed md:leading-relaxed sm:leading-relaxed font-medium lg:text-2xl md:text-2xl sm:text-2xl relative left-0 bottom-9 lg:pb-0 sm:pb-8">
+            {t('intro3')}
             </p>
           </div>
         </div>
@@ -49,7 +51,7 @@ const t = useTranslations('home');
         </div>
       </div>
       <div className="w-full flex justify-center mt-6">
-            <SocialMediaNew />
+            <SocialMedia />
 
           </div>      
           </FadeIn>
