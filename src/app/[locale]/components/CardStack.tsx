b3daa2d9e9d9de4912440b735f59aef35c619fce
HaniChihabi <hanichihabih@gmail.com>
2024-03-26 16:00:05 +0100

    added t cardstack

--- src/app/[locale]/components/CardStack.tsx
+++ src/app/[locale]/components/CardStack.tsx
@@ -4,123 +4,54 @@ import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import Image from "next/image";
 import { useTranslations } from "next-intl";
+
 export type Card = {
   id: number;
   name: string;
   designation: string;
-  content: React.ReactNode;
+  imageSrc: string; // Adding image source here
+  contentKey: string; // Use keys for translation instead of direct content
 };
+
+// Adjusted CARDS array to use content keys
 const CARDS: Card[] = [
   {
     id: 0,
     name: "John Smith",
     designation: "CEO, Smith Enterprises",
-    content: (
-      <div>
-        <div className="flex items-center">
-          <div className="relative w-20 h-20 mr-4 mb-10">
-            <Image
-              src="/pp1.jpg"
-              alt="John Smith"
-              layout="fill"
-              className="rounded-xl"
-            />
-          </div>
-        </div>
-        <p className="text-2xl mb-20">
-        </p>
-      </div>
-    ),
+    imageSrc: "/pp1.jpg",
+    contentKey: "cardContentJohnSmith",
   },
   {
     id: 1,
     name: "Alice Johnson",
     designation: "Marketing Director, Johnson Co.",
-    content: (
-      <div>
-        <div className="flex items-center">
-        <div className="relative w-20 h-20 mr-4 mb-10">
-            <Image
-              src="/pp2.jpg"
-              alt="Alice Johnson"
-              layout="fill"
-              className="rounded-xl"
-            />
-          </div>
-        </div>
-        <p className="text-2xl leading-9 font-normal">
-          Your website design skills are exceptional. It looks stunning and functions flawlessly. Thank you!
-        </p>
-      </div>
-    ),
+    imageSrc: "/pp2.jpg",
+    contentKey: "cardContentAliceJohnson",
   },
   {
     id: 2,
-    name: "Michael Brown",
-    designation: "CTO, Brown Innovations",
-    content: (
-      <div>
-        <div className="flex items-center">
-        <div className="relative w-20 h-20 mr-4 mb-10">
-            <Image
-              src="/pp3.jpg"
-              alt="Michael Brown"
-              layout="fill"
-              className="rounded-xl"
-            />
-          </div>
-        </div>
-        <p className="text-2xl">
-          The website you developed for our company is outstanding. It has streamlined our processes and captivated our audience. Thank you!
-        </p>
-      </div>
-    ),
+    name: "Michael Turner",
+    designation: "CTO, Turner Innovations",
+    imageSrc: "/pp3.jpg",
+    contentKey: "cardContentMichaelTurner",
   },
   {
     id: 3,
-    name: "Emily Wilson",
-    designation: "Founder, Wilson Creations",
-    content: (
-      <div>
-        <div className="flex items-center">
-        <div className="relative w-20 h-20 mr-4 mb-10">
-            <Image
-              src="/pp4.jpg"
-              alt="Emily Wilson"
-              layout="fill"
-              className="rounded-xl"
-            />
-          </div>
-        </div>
-        <p className="text-2xl leading-9">
-          Your expertise in website development is remarkable. The website captures our brand identity perfectly. Thank you!
-        </p>
-      </div>
-    ),
+    name: "Emily Carter",
+    designation: "Founder, Carter Designs",
+    imageSrc: "/pp4.jpg",
+    contentKey: "cardContentEmilyCarter",
   },
   {
     id: 4,
     name: "David Lee",
     designation: "Director of Operations, Lee Corp",
-    content: (
-      <div>
-        <div className="flex items-center justify-between">
-        <div className="relative w-20 h-20 mr-4 mb-10">
-            <Image
-              src="/pp5.jpg"
-              alt="David Lee"
-              layout="fill"
-              className="rounded-xl"
-            />
-          </div>
-        </div>
-        <p className="text-2xl leading-9">
-          Working with you on our website project has been a pleasure. The website delivered has exceeded our expectations. Thank you!
-        </p>
-      </div>
-    ),
-  },
+    imageSrc: "/pp5.jpg",
+    contentKey: "cardContentDavidLee",
+  }
 ];
+
 
 export const CardStack = ({
   items,
@@ -141,9 +72,10 @@ export const CardStack = ({
   const SCALE_FACTOR = scaleFactor || 0.006;
   const CARD_WIDTH = cardWidth || 450; // Default card width
   const CARD_HEIGHT = cardHeight || 560; // Default card height
-  const CONTENT_PADDING = contentPadding || "30px"; // Default content padding
+  const CONTENT_PADDING = contentPadding || "40px"; // Default content padding
 
   const [cards, setCards] = useState<Card[]>(items);
+  const t = useTranslations('Cards'); // Assuming your namespace for cards is 'Cards'
 
   const handleClick = (clickedId: number) => {
     setCards((prevCards) => {
@@ -156,56 +88,48 @@ export const CardStack = ({
 
   return (
     <div className="relative flex items-center justify-center h-auto w-auto">
-      {cards.map((card, index) => {
-        return (
-          <motion.div
-            key={card.id}
-            className="absolute dark:bg-black bg-white h-60 w-60 md:h-80 md:w-96 rounded-3xl p-4 shadow-xl border border-neutral-200 dark:border-white/[0.1]  shadow-black/[0.1] dark:shadow-white/[0.05] flex flex-col justify-between select-none"
-            style={{
-              userSelect: 'none', // Add this line
-              width: CARD_WIDTH,
-              height: CARD_HEIGHT,
-              top: index * CARD_OFFSET,
-              transform: `translate(-50%, -50%) scale(${
-                1 - index * SCALE_FACTOR
-              })`,
-              zIndex: cards.length - index,
-            }}
-            onClick={() => handleClick(card.id)} // Updated onClick handler
-            animate={{
-              top: index * -CARD_OFFSET,
-              scale: 1 - index * SCALE_FACTOR,
-              zIndex: cards.length - index,
-            }}
-          >
-            <div className="font-normal text-neutral-700 dark:text-neutral-200 "
-              style={{ padding: CONTENT_PADDING }}
-            >
-              <div >
-                {card.content}
-              </div>
-              <div className="absolute bottom-7">
-                <p className="text-neutral-500 font-medium dark:text-white">
-                  {card.name}
-                </p>
-                <p className="text-neutral-400 font-normal dark:text-neutral-200">
-                  {card.designation}
-                </p>
-              </div>
-            </div>
-          </motion.div>
-        );
-      })}
+      {cards.map((card, index) => (
+        <motion.div
+          key={card.id}
+          className="absolute dark:bg-black bg-white h-60 w-60 md:h-80 md:w-96 rounded-3xl p-4 shadow-xl border border-neutral-200 dark:border-white/[0.1]  shadow-black/[0.1] dark:shadow-white/[0.05] flex flex-col justify-between select-none"
+          style={{
+            userSelect: 'none',
+            width: CARD_WIDTH,
+            height: CARD_HEIGHT,
+            top: index * CARD_OFFSET,
+            transform: `translate(-50%, -50%) scale(${1 - index * SCALE_FACTOR})`,
+            zIndex: cards.length - index,
+            padding: CONTENT_PADDING
+          }}
+          onClick={() => handleClick(card.id)}
+          animate={{
+            top: index * -CARD_OFFSET,
+            scale: 1 - index * SCALE_FACTOR,
+            zIndex: cards.length - index,
+          }}
+        >
+          <div className="relative h-20 w-20 bg-black rounded-xl">
+          <Image src={card.imageSrc} alt={card.name} layout="fill" className="rounded-xl" /></div>
+          <div >
+            <p className="text-xl">{t(card.contentKey)}</p>
+          </div>
+          <div>
+            <p>{card.name}</p>
+            <p>{card.designation}</p>
+          </div>
+        </motion.div>
+      ))}
     </div>
   );
 };
 
 export default function CardStackDemo() {
-  const t = useTranslations('rating');
+  const t = useTranslations('rating'); // Assuming your namespace for the home page is 'HomePage'
+
   return (
     <div className="flex flex-col items-center justify-center w-full font-medium">
-      <h1 className="text-7xl">{t('Text1')}</h1>
-      <h1 className="text-7xl mb-40 text-center">{t('Text2')}</h1>
+      <h1 className="text-7xl">{t('Text1')}</h1> {/* Example usage of `t` */}
+      <h1 className="text-7xl mb-44">{t('Text2')}</h1> {/* Example usage of `t` */}
       <CardStack items={CARDS} />
     </div>
   );
